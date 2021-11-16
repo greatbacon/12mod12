9e2d275f213ec5d2cd9f1e771610ed64b6a1e93b
greatbacon <hamiltonjreed@gmail.com>
2021-11-15 19:54:01 -0700

    Added more women quotes

--- src/lib/data/quotes.ts
+++ src/lib/data/quotes.ts
@@ -77,58 +77,182 @@ export const quotes = [
 	},
 	{
 		author: 'Vera Rubin',
-		text: 'Still more mysteries of the universe remain hidden. Their discovery awaits the adventurous scientists of the future. I like it this way.',		
+		text: 'Still more mysteries of the universe remain hidden. Their discovery awaits the adventurous scientists of the future. I like it this way.',
 	},
 	{
 		author: 'Dorothy Hodgkin',
-		text: 'I was captured for life by chemistry and by crystals.'
+		text: 'I was captured for life by chemistry and by crystals.',
 	},
-  {
+	{
 		author: 'Chien-Shiung Wu',
 		text: 'The main stumbling block in the way of any progress is and always has been unimpeachable tradition.',
 	},
 	{
 		author: 'Mamie Phipps Clark',
-		text: "I began to think that a crucial part of children's lives, no matter what happens, has to be a degree of security and acceptance."		
+		text: "I began to think that a crucial part of children's lives, no matter what happens, has to be a degree of security and acceptance.",
 	},
 	{
 		author: 'Alice Ball',
-		text: "I work and I work and still it seems that I have done nothing.",
+		text: 'I work and I work and still it seems that I have done nothing.',
 	},
 	{
 		author: 'Barbara McClintock',
-		text: "When you have that joy, you do the right experiments. You let the material tell you where to go, and it tells you at every step what the next has to be...",
+		text: 'When you have that joy, you do the right experiments. You let the material tell you where to go, and it tells you at every step what the next has to be...',
 	},
 	{
 		author: 'Gerty Cori',
-		text: "As a researcher the unforgotten moments of my life are rare ones...When the veil over nature's secrets seems to suddenly lift."
+		text: "As a researcher the unforgotten moments of my life are rare ones...When the veil over nature's secrets seems to suddenly lift.",
 	},
 	{
 		author: 'Rita Levi-Montalcini',
-		text: "Above all, don't fear difficult moments. The best comes from them."
+		text: "Above all, don't fear difficult moments. The best comes from them.",
 	},
 	{
 		author: 'Lise Meitner',
-		text: "Life need not be easy, provided only it was not empty."
+		text: 'Life need not be easy, provided only it was not empty.',
 	},
 	{
 		author: 'Sau Lan Wu',
-		text: "Live with integrity and let your conscience be your guide. Be a pioneer and follow your heart, contributing to future humankind.",
+		text: 'Live with integrity and let your conscience be your guide. Be a pioneer and follow your heart, contributing to future humankind.',
 	},
 	{
 		author: 'Patricia Bath',
-		text: "Believe in the power of truth. Do not allow your mind to be imprisoned by majority thinking.",		
+		text: 'Believe in the power of truth. Do not allow your mind to be imprisoned by majority thinking.',
 	},
 	{
 		author: 'Cecilia Payne-Gaposchkin',
-		text: "There is no joy more intense than that of coming upon a fact that cannot be understood in terms of currently accepted ideas.",
+		text: 'There is no joy more intense than that of coming upon a fact that cannot be understood in terms of currently accepted ideas.',
 	},
 	{
 		author: 'Karen Horney',
-		text: "Fortunately analysis is not the only way to resolve inner conflicts. Life itself still remains a very effective therapist.",
+		text: 'Fortunately analysis is not the only way to resolve inner conflicts. Life itself still remains a very effective therapist.',
 	},
 	{
 		author: 'Elizabeth Blackwell',
 		text: "If the present arrangements of society will not admit of woman's free development, then society must be remodelled.",
-	},	
+	},
+	{
+		author: 'Esther Lederberg',
+		text: 'You can begin anytime, even though it takes a lifetime to be good.',
+	},
+	{
+		author: 'Wang Zhenyi',
+		text: "It's made to believe/women are the same as men; Are you not convinced/daughters can also be heroic?",
+	},
+	{
+		author: 'Maria Sibylla Merian',
+		text: 'Art and nature shall always be wrestling until they eventually conquer one another so that the victory is the stroke and line.',
+	},
+	{
+		author: 'May-Britt Moser',
+		text: "A good designer has a lot in common with a good researcher. Both hunt for excellence and perfection. And you have to really focus on the details, and you don't really know what the final result will be before you have it.",
+	},
+	{
+		author: 'Joan Beauchamp Proctor',
+		text: "Why shouldn't a woman run a reptile house? Women are at work in my country, and the rest of the world, in all types of work and professions.",
+	},
+	{
+		author: 'Elizabeth Blackburn',
+		text: "Don't be araid to ask people for help -- and then feel free to ignore it!",
+	},
+	{
+		author: 'Mae Jemison',
+		text: 'The first thing about empowerment is to understand that you have the right to be involved. The second one is that you have something important to contribute. And the third piece is that you have to take the risk to contribute it.',
+	},
+	{
+		author: 'Hedy Lamarr',
+		text: 'My father made me understand that I must make my own decisions, mold my own character, think my own thoughts.',
+	},
+	{
+		author: 'Katherine Johnson',
+		text: "The other women didn't ask questions or take the task any further. I asked questions; I wanted to know why. They got used to me asking questions and being the only woman there.",
+	},
+	{
+		author: 'Katia Krafft',
+		text: 'For me the danger is not important... on volcanoes I forget everything.',
+	},
+	{
+		author: 'Maryam Mirzakhani',
+		text: 'You have to spend some energy and effort to see the beauty of math.',
+	},
+	{
+		author: 'Hertha Aryton',
+		text: 'An error that ascribes to a man what was actually the work of a woman has more lives than a cat.',
+	},
+	{
+		author: 'Grace Hopper',
+		text: 'People are allergic to change. You have to get out and sell the idea.',
+	},
+	{
+		author: 'Jocelyn Bell Burnell',
+		text: "If we assume we've arrived at absolute truth, we stop searching. We stop developing.",
+	},
+	{
+		author: 'Valentina Tereshkova',
+		text: 'Once you are at this faraway distance in space, you realize the significance of what it is that unites us. Let us work together to overcome our differences.',
+	},
+	{
+		author: 'Sylvia Earle',
+		text: 'No water, no life. No blue, no green.',
+	},
+	{
+		author: 'Ada Lovelace',
+		text: 'Imagination is the discovering faculty, pre-eminently. It is that which penetrates into the unseen worlds around us, the worlds of science.',
+	},
+	{
+		author: 'Marie Curie',
+		text: 'I was taught that the way of progress is neither swift nor easy.',
+	},
+	{
+		author: 'Rachel Carson',
+		text: 'The human race is challenged more than ever before to demonstrate our mastery, not over nature but of ourselves.',
+	},
+	{
+		author: 'Maria Goeppert-Mayer',
+		text: 'When you love science, all you really want is to keep working.',
+	},
+	{
+		author: 'Edith Clarke',
+		text: "There is no demand for women engineers, as such, as there are for women doctors; but there's always a demand for anyone who can do a good piece of work.",
+	},
+	{
+		author: 'Emmy Noether',
+		text: 'My methods are really methods of working and thinking; This is why they have crept in everywhere anonymously.',
+	},
+	{
+		author: 'Lillian Gilbreth',
+		text: 'We considered our time too valuable to be devoted to actual labor in the home. We were executives.',
+	},
+	{
+		author: 'Mary Agnes Chase',
+		text: 'Grass made it possible for the human race to abandon cave life & follow herds.',
+	},
+	{
+		author: 'Rosalyn Yalow',
+		text: 'We must believe in ourselves or no one else will believe in us; We must match our aspirations with the competence, courage, and determination to suceed.',
+	},
+	{
+		author: 'Rosalind Franklin',
+		text: 'Science and everyday life cannot and should not be separated.',
+	},
+	{
+		author: 'Gertrude Elion',
+		text: "Don't let others discourage you or tell you that you can't do it. In my day I was told women didn't go into chemistry. I saw no reason why we couldn't.",
+	},
+	{
+		author: 'Jane Goodall',
+		text: 'Only when our clever brain and our human heart work together can we reach our full potential.',
+	},
+	{
+		author: 'Florence Bascom',
+		text: 'I have considerable pride in the fact that some of the best work done in geology today by women, ranking with that done by men, has been done by my students.',
+	},
+	{
+		author: 'Annie Easley',
+		text: "Nothing was given to minorities or women. It took some fighting to get that equal opportunity, and we're still fighting today.",
+	},
+	{
+		author: 'Christiane Nusslein-Volhard',
+		text: 'I immediately loved working with flies. They fascinated me, and followed me around in my dreams.',
+	},
 ];
