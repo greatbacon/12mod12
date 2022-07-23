230083cd382ffb642f7688c2a79936c3dcc42aac
greatbacon <hamiltonjreed@gmail.com>
2022-07-23 17:34:29 -0600

    Missed leading slashes

--- src/lib/data/numismatics.ts
+++ src/lib/data/numismatics.ts
@@ -10,8 +10,8 @@ export const codes = {
 
 export const monies = [
   {
-    obverse:"numismatics-img/640px-US_$2_bill_obverse_series_2003_A.jpg",
-    reverse:"numismatics-img/640px-US_$2_bill_reverse_series_2003_A.jpg",
+    obverse:"/numismatics-img/640px-US_$2_bill_obverse_series_2003_A.jpg",
+    reverse:"/numismatics-img/640px-US_$2_bill_reverse_series_2003_A.jpg",
     name:"United States Federal Reserve Note two-dollar bill",
     code: codes[`USD`],
     issued: 1976,
@@ -20,8 +20,8 @@ export const monies = [
     link: "https://en.wikipedia.org/wiki/United_States_two-dollar_bill",
   },
   {
-  obverse:"numismatics-img/480px-2021-P_US_Quarter_Obverse.jpg",
-  reverse:"numismatics-img/488px-1976_Bicentennial_Quarter_Rev.png",
+  obverse:"/numismatics-img/480px-2021-P_US_Quarter_Obverse.jpg",
+  reverse:"/numismatics-img/488px-1976_Bicentennial_Quarter_Rev.png",
   name:"United States Bicentennial Washington quarter coin",
   code: codes[`USD`],
   issued: 1976,
@@ -30,8 +30,8 @@ export const monies = [
   link: "https://en.wikipedia.org/wiki/United_States_Bicentennial_coinage",
 },
 {
-  obverse:"numismatics-img/480px-2021-P_US_Quarter_Obverse.jpg",
-  reverse:"numismatics-img/2014-ATB-Proof-Arches-rev-200.png",
+  obverse:"/numismatics-img/480px-2021-P_US_Quarter_Obverse.jpg",
+  reverse:"/numismatics-img/2014-ATB-Proof-Arches-rev-200.png",
   name:"United States America the Beautiful Utah Washington quarter coin",
   code: codes[`USD`],
   issued: 2014,
@@ -40,8 +40,8 @@ export const monies = [
   link: "https://en.wikipedia.org/wiki/America_the_Beautiful_quarters",
 },
 {
-  obverse:"numismatics-img/480px-2021-P_US_Quarter_Obverse.jpg",
-  reverse:"numismatics-img/2014-ATB-Proof-Great-Sand-Dunes-rev-200.png",
+  obverse:"/numismatics-img/480px-2021-P_US_Quarter_Obverse.jpg",
+  reverse:"/numismatics-img/2014-ATB-Proof-Great-Sand-Dunes-rev-200.png",
   name:"United States America the Beautiful Colorado Washington quarter coin",
   code: codes[`USD`],
   issued: 2014,
