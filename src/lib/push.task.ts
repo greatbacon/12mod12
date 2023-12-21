670885874c29c54ca2ef8eedecfd57f82912bec2
hamiltonjreed <hamiltonjreed@gmail.com>
2023-12-20 22:54:25 -0700

    rename deploy to push

--- src/lib/push.task.ts
+++ src/lib/push.task.ts
@@ -1,19 +1,19 @@
-import type {Task} from '@feltjs/gro';
-import {spawn} from '@feltjs/util/process.js';
-import {DIST_DIRNAME} from '@feltjs/gro/dist/paths.js';
+import type {Task} from '@grogarden/gro';
+import {spawn} from '@grogarden/util/process.js';
+import {SVELTEKIT_DIST_DIRNAME} from '@grogarden/gro/paths.js';
 
 const STATIC_REPO = '../greatbacon.github.io';
 
 export const task: Task = {
 	summary: 'deploy static website to prod',
 	production: true,
-	run: async ({invokeTask}) => {
-		await invokeTask('clean');
-		await invokeTask('build');
+	run: async ({invoke_task}) => {
+		await invoke_task('clean');
+		await invoke_task('build');
 		const timestamp = Date.now();
 
 		await spawn('git', [`-C`, `${STATIC_REPO}`, `pull`]);
-		await spawn('cp', [`-r`, `${DIST_DIRNAME}/svelte-kit/.`, `${STATIC_REPO}`]);
+		await spawn('cp', [`-r`, `${SVELTEKIT_DIST_DIRNAME}/svelte-kit/.`, `${STATIC_REPO}`]);
 		await spawn('git', [`-C`, `${STATIC_REPO}`, `add`, `.`]);
 		await spawn('git', [`-C`, `${STATIC_REPO}`, `commit`, `-m`, ` "new deploy ${timestamp}"`]);
 		await spawn('git', [`-C`, `${STATIC_REPO}`, `push`]);
