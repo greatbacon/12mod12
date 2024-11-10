44bfe908f18260727e1eba843e55a43c0fffb660
greatbacon <4512607+greatbacon@users.noreply.github.com>
2024-11-10 13:28:13 -0700

    Update the about page and code page (#7)

--- src/lib/data/code.ts
+++ src/lib/data/code.ts
@@ -2,14 +2,23 @@ import type {Blurb} from '$lib/website';
 
 export const code: Blurb[] = [
 	{
+		title: 'Loom',
+		content: `Since Felt was developed as an open-source project, 
+		I forked from where it was at in Winter of 2023 & have continued the work of building a programmable social platform. This new iteration is called Loom.`,    
+		start: '2024-01-01',		
+		thumbnail: '/portfolio/img/loom_logo.png',
+		link: 'https://getloom.org',
+	},
+	{
 		title: 'Felt',
 		content: `In the winter of 2020 I began coversations about a new software & business project with my business partner <a href="https://ryanatkn.com">Ryan</a>.     
-    We are developing a tool for building & maintaing digital communities on the smaller scale.     
-    In the process we also hope to establish a worker-user owned platform cooperative. 
-    In the spring of 2021 I quit my paying job to start working on it full-time.
-    It is an open source project that can be viewed <a href="https://github.com/feltcoop/felt-server">here</a>.`,
+    We began developing a tool for building & maintaing digital communities on the smaller scale.     
+    In the process we also hoped to establish a worker-user owned platform cooperative. 
+    In the spring of 2021 I quit my paying job to start working on it full-time. 
+	Due to life circumstances & creative differences, the partnership (and the project) ended at the end of 2023.`,    
 		start: '2020-01-01',
-		thumbnail: 'https://www.felt.social/felt.png',
+		wrapped: '2023-12-31',
+		thumbnail: '/portfolio/img/felt.png',
 		link: 'https://felt.social',
 	},
 	{
@@ -25,7 +34,7 @@ export const code: Blurb[] = [
 	{
 		title: '12MOD12',
 		content:
-			'This website is perhaps my longest, and simultaneously most recent project as of the time of this writing.  Originally this website just served a few static HTML pages.  Over time it has grown to host most of my professional and amateur work over the last 10 years.  As of this most recent iteration, the website is a static site built with the Sveltekit framework & Felt design library.',
+			'This website is perhaps my longest, and simultaneously most recent project as of the time of this writing.  Originally this website just served a few static HTML pages.  Over time it has grown to host most of my professional and amateur work over the last 10+ years.  As of this most recent iteration, the website is a static site built with the Sveltekit framework & Felt design library.',
 		start: '2012-08-01',
 		thumbnail: '/portfolio/img/modtwelve.png',
 		link: 'https://github.com/greatbacon/12mod12',
