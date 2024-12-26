4714506bc187b8ad8d00f1428e22b9b7694b4e6e
greatbacon <4512607+greatbacon@users.noreply.github.com>
2024-12-26 16:52:17 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { page } from "$app/stores";    
+</script>
+
+<h1>Welcome to Your New Loom Site!</h1>
+<p>Visit <a href="https://hub.{$page.url.hostname}">your new loom instance</a> to get started and register your admin account</p>
