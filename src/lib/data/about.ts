be78ee04dada3d938c1b704ad47a59e8afd127b4
greatbacon <hamiltonjreed@gmail.com>
2024-12-26 17:47:13 -0700

    Got everything but RSS gen set up

---
+++ src/lib/data/about.ts
@@ -0,0 +1,26 @@
+import type {Blurb} from '$lib/website';
+
+export const about: Blurb[] = [
+	{
+		title: 'Hamilton J Reed',
+		subtitle: 'Maintaining this website since...',
+		content: `
+    <p>Hamilton Reed is a professional software developer with over a decade of experience in the industry building cloud native web services that meet customer needs at scale. Or put another way, he makes sure the webpages and mobile apps of the world always have a stable, steady webserver to talk to on the backend.
+
+    <br><br>A graduate of Colorado State University, while there he pursued a Bachelor of Science in Computer Science and a minor in English. Originally driven to the field of Computer Science by his love of video games and desire to develop them, he has since discovered a joy in the creative problem solving that comes with software engineering. That and the video game industry is explotative as hell and wants people to work 80 hour weeks under constant crunch.        
+					
+    <br><br>Seeking, analyzing, and designing systems and methods for observing and modifying the world is one of the driving goals of Hamilton's career.  Particularly fascinating to him are systems that model and control the economics of the world (i.e. finance & logistics) or facilitate democratic & social self-organization.
+            
+    <br><br>In his free time he also enjoys reading & writing all manner of things as well as producing podcasts.  While currently no more than a hobby he feels it helps enhance his capacity for clear and coherent communication in all aspects of his life. He's also fond of desiginging, fabricating, and playing board games.
+
+    <br><br>In the beginning of 2021 Hamilton quit his job to work full time on a new programmable social platform. Although no longer working on it full time, you can find out more <a href="https://getloom.org">about Loom here</a>.
+            
+    <br><br><a class="links" href="https://getloom.org/">LOOM</a>
+    <br><br><a class="links" href="https://github.com/greatbacon">GITHUB</a>
+    <br><br><a class="links" href="https://hachyderm.io/@greatbacon">MASTODON</a>    
+    <br><br><a class="links" href="https://twitter.com/greatbaconbits">TWITTER</a>        
+    `,
+		start: '2012-08-19',
+		thumbnail: '/portfolio/img/headshot.jpg',
+	},
+];
---
+++ src/lib/data/audio.ts
@@ -0,0 +1,68 @@
+import type {Blurb} from '$lib/website';
+
+export const audio: Blurb[] = [
+	{
+		title: 'Hollywood Zoo Crew',
+		subtitle: 'an improv comedy podcast',
+		content:
+			'A comedy improv podcast, here to provide all the 100% true latest news, haps, interviews, and reviews of what goes on in Hollywood, outside of it.<br> Project is finished for now, pending any bolts of inspiritaion.',
+		link: '/audio/hollywoodzoocrew',
+		start: '2017-04-25',
+		wrapped: '2018-05-20',
+		thumbnail: '/portfolio/img/HZC_smol.jpg',
+	},
+	{
+		title: 'Hary Potter and the Portrait of What Looked Like a Large Pile of Ash',
+		subtitle: 'Chapter 13: The Handsome One',
+		content: `<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/369295169&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>`,
+		start: '2017-12-13',
+	},
+	{
+		title: '006 Liberia The Civil Wars',
+		subtitle: 'A Line on the Map',
+		content: `<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/307180699&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>`,
+		start: '2016-10-26',
+	},
+	{
+		title: '005 Liberia The Language',
+		subtitle: 'A Line on the Map',
+		content: `<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/307179819&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>`,
+		start: '2016-10-20',
+	},
+	{
+		title: '004 Liberia The History',
+		subtitle: 'A Line on the Map',
+		content: `<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/307179525&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>`,
+		start: '2016-10-19',
+	},
+	{
+		title: '003 Afghanistan The Last Hundred Years',
+		subtitle: 'A Line on the Map',
+		content: `<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/307179322&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>`,
+		start: '2016-10-18',
+	},
+	{
+		title: '002 Afghanistan the First Hundred Years',
+		subtitle: 'A Line on the Map',
+		content: `<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/307176275&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>`,
+		start: '2016-10-11',
+	},
+	{
+		title: '001 An Intro To Afghanistan',
+		subtitle: 'A Line on the Map',
+		content: `<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/307173901&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>`,
+		start: '2016-10-05',
+	},
+	{
+		title: 'West Mesa Desert',
+		content: `<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/265633928&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>`,
+		start: '2016-05-23',
+	},
+	{
+		title: 'Impressions Of America',
+		subtitle: 'Gutenberg on Tape',
+		content:
+			'<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/259954216&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>',
+		start: '2016-04-19',
+	},
+];
