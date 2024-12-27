e4786de935baf7871b7f7edc9b48f921fd20a6c4
greatbacon <hamiltonjreed@gmail.com>
2024-12-26 17:13:51 -0700

    Front page works!

---
+++ src/lib/website.ts
@@ -0,0 +1,15 @@
+export interface Blurb {
+	title: string;
+	subtitle?: string;
+	content: string;
+	start: string;
+	wrapped?: string;
+	thumbnail?: string;
+	link?: string;
+}
+
+export interface Quote {
+	author: string;
+	text: string;
+	link: string;
+}
